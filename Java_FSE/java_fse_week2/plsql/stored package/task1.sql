dfdb9297999b7a2f229e8fdea97f5079a978032b
Jananih-04 <144415558+Jananih-04@users.noreply.github.com>
2024-08-07 23:01:39 +0530

    Delete Java_FSE/java_fse_week2/plsql/stored package directory

--- Java_FSE/java_fse_week2/plsql/stored package/task1.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE PROCEDURE ProcessMonthlyInterest IS
-BEGIN
-    UPDATE Accounts
-    SET Balance = Balance * 1.01
-    WHERE AccountType = 'Savings';
-    COMMIT;
-END ProcessMonthlyInterest;
